e4e5559df3b8125e79d2a6fae6490a97e6a4c2df
maksiurino <153639807+maksiurino@users.noreply.github.com>
2026-03-26 18:51:59 +0100

    Adding Features

--- src/main/java/net/maxrhino/tlgt/util/screens/TestScreen.java
+++ src/main/java/net/maxrhino/tlgt/util/screens/TestScreen.java
@@ -5,6 +5,7 @@ import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 
 public class TestScreen extends Screen {
     private int imageWidth;
@@ -26,14 +27,27 @@ public class TestScreen extends Screen {
     public void extractBackground(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a) {
         super.extractBackground(graphics, mouseX, mouseY, a);
 
+        int x = (this.width - this.imageWidth) / 2;
+        int y = (this.height - this.imageHeight) / 2;
+
         graphics.blit(
                 RenderPipelines.GUI_TEXTURED,
-                TheLeakedGUITrue.id("textures/gui/containers/barrel.png"),
-                (this.width - this.imageWidth) / 2,
-                (this.height - this.imageHeight) / 2,
+                TheLeakedGUITrue.id("textures/gui/containers/default.png"),
+                x,
+                y,
                 0, 0,
                 this.imageWidth, this.imageHeight,
                 this.imageWidth, this.imageHeight
+        );
+
+        graphics.blit(
+                RenderPipelines.GUI_TEXTURED,
+                Identifier.withDefaultNamespace("textures/gui/sprites/container/slot.png"),
+                x + 8,
+                y + 142,
+                1, 1,
+                16, 16,
+                16, 16
         );
     }
 }
